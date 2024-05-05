ef513108e58ee03e14da706b3311f37f4b8ffbea
Supratim Dey <supratimdey@outlook.com>
2024-05-05 22:16:58 +0530

    added next ui to the project successfully

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <div className="text-3xl bg-gray-300  max-h-screen min-h-screen">
+      About page
+    </div>
+  );
+}
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import { Button, Link } from "@nextui-org/react";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello World</div>
+      <div>
+        <Link
+          color="secondary"
+          showAnchorIcon
+          isExternal
+          href="/about"
+          as={NextLink}
+          size="lg"
+        >
+          About
+        </Link>
+      </div>
+      <Button size="lg" color="secondary" variant="shadow">
+        Click Me
+      </Button>
+      <Button className="bg-red-200" endContent={<p>✅</p>}>
+        Click Me
+      </Button>
     </main>
   );
 }
